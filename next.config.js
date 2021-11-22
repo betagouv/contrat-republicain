2a33da25f1a370b8b56991eaef842c51c72a6558
Calev Eliacheff <c.eliacheff@gmail.com>
2021-11-22 20:31:57 +0100

    Add static local fonts

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(woff|woff2|ttf|eot|svg)$/,
-      loader: "file-loader",
-      options: {
-        esModule: false,
-        name: "[name].[ext]",
-        outputPath: "static/fonts/",
-        publicPath: "../fonts/",
-      },
-    });
-
-    return config;
-  },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-//import '@gouvfr/dsfr/dist/dsfr/dsfr.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
